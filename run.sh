09b93c580ad74d6a9a2091c359063c0efe2ae040
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-05-11 00:11:31 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm i 24
+nvm use 24
+
 npm run install:all
 
 
